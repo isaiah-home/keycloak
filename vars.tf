5de37c7a65d8ce673ba80d821ca1c52ba503c6f4
isaiah-v <isaiah.v@comcast.net>
2022-05-09 02:40:57 +0000

    removed default variables

--- vars.tf
+++ vars.tf
@@ -7,44 +7,31 @@ variable "install_root" {
 # admin
 variable "db_admin_name" {
   type    = string
-  default = "admin"
 }
 variable "db_admin_password" {
   type    = string
-  default = "password"
 }
 
 # --== Keycloak ==-- #
 # keycloak
 variable "db_keycloak_name" {
   type    = string
-  default = "keycloak"
 }
 variable "db_keycloak_password" {
   type    = string
-  default = "password"
 }
 variable "keycloak_user" {
   type    = string
-  default = "keycloak"
 }
 variable "keycloak_password" {
   type    = string
-  default = "password"
 }
 
 # --== WikiJS ==-- #
 variable "db_wikijs_name" {
   type    = string
-  default = "wikijs"
 }
 variable "db_wikijs_password" {
   type    = string
-  default = "password"
 }
 
-# --== Nginx ==-- #
-variable "domain" {
-  type    = string
-  default = "ivcode.org"
-}
