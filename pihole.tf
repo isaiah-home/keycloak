6fcd5eaa77d3da79aa75cdbf2615dccc8602f67e
isaiah-v <isaiah.v@comcast.net>
2023-10-02 08:11:08 -0700

    updated pihole config

--- pihole.tf
+++ pihole.tf
@@ -1,12 +1,13 @@
 resource "docker_image" "pihole" {
-  name = "pihole/pihole:2023.03.1"
+  name = "pihole/pihole:2023.05.2"
   keep_locally = true
 }
 
 resource "docker_container" "pihole" {
-  image   = docker_image.pihole.image_id
-  name    = "organize-me-pihole"
-  restart = "unless-stopped"
+  image    = docker_image.pihole.image_id
+  name     = "organize-me-pihole"
+  hostname = "pihole"
+  restart  = "unless-stopped"
   env = [
     "TZ=${var.timezone}",
     "WEBPASSWORD=${data.aws_ssm_parameter.pihole_webpassword.value}"
@@ -18,6 +19,11 @@ resource "docker_container" "pihole" {
   volumes {
     container_path = "/etc/dnsmasq.d"
     host_path      = "${var.install_root}/pihole/etc/dnsmasq.d"
+  }
+  networks_advanced {
+    name    = docker_network.organize_me_network.name
+    aliases = ["pihole"]
+    ipv4_address = "172.22.0.15"
   }
   ports {
     internal = 53
